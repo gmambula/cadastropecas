63ef2a7ae4286bd4d59280fca44978ae54b445ab
gmambula <101022003+gmambula@users.noreply.github.com>
2022-03-20 16:03:34 -0300

    segundo commit

---
+++ cadastropecas.js
@@ -0,0 +1,24 @@
+// algoritimopecas
+
+let pesoPeca = "180"
+let listaPeca = ["lanterna" , "pisca" ,"pastilha"]
+let nomePeca = listaPeca.length
+
+nomePeca = "direcao"
+
+if(pesoPeca > 100)
+    console.log("podemos cadastrar")
+    else{
+    console.log("não podemos cadastrar")
+    }
+if (nomePeca >=10) {
+    console.log("capacidade maxima atingida")
+} else {
+    console.log("ainda há espeço")    
+}
+if (nomePeca < 3) {
+    console.log("nome invalido")
+    } else {
+    console.log("nome da peca adequado")
+}
+     
